2a468832311e5f37f7a51110843e7d5dff6d43e9
filipandersson3 <filip.andersson3@elev.ga.ntig.se>
2021-10-04 11:00:08 +0200

    🎉 kopierat eleventy och sass exempel med lite dependencies

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addWatchTarget("src/images");
+
+    eleventyConfig.addShortcode('image', (src, alt) => `<img alt="${alt}" src="${src}">`);
+
+    return {
+        dir: {
+            input: 'src',
+            output: 'dist'
+        }
+    };
+};
---
+++ src/index.md
@@ -0,0 +1,20 @@
+---
+title: Sammanfattning av det vi gjort
+layout: base.njk
+---
+
+# {{ title | safe }}
+
+# {{ meta.title | safe }}
+
+# Tjenamosch
+## underrubjirk
+### underrubjirk
+#### underrubjirk
+##### underrubjirk
+
+Din gamla galosch.
+
+![Min bild](/images/elonmusk.PNG)
+
+{% image "/images/elonmusk.PNG", "Bild" %}
